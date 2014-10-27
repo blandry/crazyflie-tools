25a8e5ea7bdabf91e2ec158e87df53b05d956bc8
Benoit Landry <landry@mit.edu>
2014-10-27 11:49:32 -0400

    install lcm types using distutils

--- lcm/install_types.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "export PYTHONPATH=\$PYTHONPATH:${PWD}/crazyflie_t" >> ~/.bashrc
-echo "export CLASSPATH=\$CLASSPATH:${PWD}/crazyflie_t.jar" >> ~/.bashrc
-source ~/.bashrc
---
+++ lcm/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='crazyflie_t',
+      version='1.0',
+      packages=['crazyflie_t'])
