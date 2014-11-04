782cf8af499eee02a761459340eb7ab0540182f4
Benoit Landry <landry@mit.edu>
2014-11-04 14:27:56 -0500

    Kf experiment

--- drake/Crazyflie.m
+++ drake/Crazyflie.m
@@ -5,9 +5,16 @@ classdef Crazyflie
     end
 
     methods
-        function run(obj, utraj)
+        function run(obj, utraj, tspan)
             utraj = setOutputFrame(utraj,obj.input_frame);
-            options.tspan = utraj.tspan;
+            if (nargin<3)
+                options.tspan = utraj.tspan;
+                if (options.tspan(1)<0)
+                    options.tspan(1) = 0;
+                end
+            else
+                options.tspan = tspan;
+            end
             options.input_sample_time = 1/obj.input_freq;
             runLCM(utraj,[],options);
         end
