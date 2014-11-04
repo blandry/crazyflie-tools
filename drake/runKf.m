e5fe3a6b5da703880a4fb0e2f02b9502dc4cecaa
Benoit Landry <landry@mit.edu>
2014-11-04 18:28:22 -0500

    Kf results

--- drake/runKf.m
+++ drake/runKf.m
@@ -1,10 +1,10 @@
 function runKf()
 Fs = 10000:5000:60000;
-dt = 1;
+dt = 3;
 cf = Crazyflie();
 for i=1:numel(Fs)
     fprintf('Input signal: %d\n',Fs(i))
-    utraj = ConstantTrajectory(repmat(Fs(i),4,1));
+    utraj = ConstantTrajectory([Fs(i);Fs(i);Fs(i);Fs(i)]);
     cf.run(utraj,[0 dt]);
     utraj = ConstantTrajectory(zeros(4,1));
     cf.run(utraj,[0 dt]);
