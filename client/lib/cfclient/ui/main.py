307f8a5977ce97186d6f8e0b0abe673223a91e7d
Benoit Landry <landry@mit.edu>
2014-11-14 12:21:49 -0500

    working lcmbridge

--- client/lib/cfclient/ui/main.py
+++ client/lib/cfclient/ui/main.py
@@ -510,6 +510,5 @@ class MainUI(QtGui.QMainWindow, main_window_class):
         QDesktopServices.openUrl(QUrl("file:///" + QDir.toNativeSeparators(sys.path[1])))
 
     def closeAppRequest(self):
-        self.lcm_bridge.destroy()
         self.close()
         sys.exit(0)
--- client/lib/cfclient/utils/lcmbridge.py
+++ client/lib/cfclient/utils/lcmbridge.py
@@ -1,5 +1,6 @@
-import threading
 import lcm
+import threading
+import struct
 
 from crazyflie_t import crazyflie_thrust_t
 from cflib.crtp.crtpstack import CRTPPacket, CRTPPort
@@ -10,8 +11,8 @@ class LCMBridge(object):
     self._cf = cf
     self._channel = channel
     self._running = threading.Event()
-    self._destroyed = threading.Event()
-    self._worker = threading.Thread(target=LCMBridge.bridge_task,args=(self._cf,self._channel,self._running,self._destroyed))
+    self._worker = threading.Thread(target=LCMBridge.bridge_task,args=(self._cf,self._channel,self._running))
+    self._worker.start()
 
   def start(self):
     self._running.set()
@@ -20,20 +21,20 @@ class LCMBridge(object):
     self._running.clear()
 
   def destroy(self):
-    self._destroyed.set()
+    # not implemented yet
+    pass
 
   @staticmethod
-  def bridge_task(cf,channel,running,destroyed):
+  def bridge_task(cf,channel,running):
     lc = lcm.LCM()
-    subscription = lc.subscribe(channel,lambda channel, data: LCMBridge.msg_handler(cf,channel,data))
-    while not(destroyed):
+    subscription = lc.subscribe(channel,lambda _channel,_data: LCMBridge.handle_msg(_channel,_data,cf))
+    while True:
       running.wait()
       lc.handle()
 
   @staticmethod
-  def msg_handler(cf,channel,data):
+  def handle_msg(channel,data,cf):
     msg = crazyflie_thrust_t.decode(data)
-    print "Received message."
     pk = CRTPPacket()
     pk.port = CRTPPort.OFFBOARDCTRL
     pk.data = struct.pack('<HHHH', int(msg.thrust1)+32768,int(msg.thrust2)+32768,int(msg.thrust3)+32768,int(msg.thrust4)+32768)
