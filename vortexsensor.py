32b3ac5051b3aa2e6f90c634a84c53348284c01b
peteflorence <pete.florence@gmail.com>
2015-04-08 18:00:15 -0400

    mapped to airspeed

--- vortexsensor.py
+++ vortexsensor.py
@@ -2,7 +2,12 @@
 
 import u3
 import lcm
+import math
 from crazyflie_t import vortex_sensor_t
+
+VOLTS_TO_PASCAL = 819 # Source: ardupilot AP_Airspeed_analog.cpp
+_ratio = 1.9936 # Source: ardupilot AP_Airspeed_analog.cpp
+_airspeed0 = 0
 
 if __name__=="__main__":
     d = u3.U3()
@@ -10,18 +15,37 @@ if __name__=="__main__":
 
     try:
 
+
+        # implement offset
+
+
+
         while True:
             msg = vortex_sensor_t()
 
             ain0bits, = d.getFeedback(u3.AIN(0)) # Read from raw bits from AIN0
             ain0Value = d.binaryToCalibratedAnalogVoltage(ain0bits, isLowVoltage=False, channelNumber=0)
-            msg.sensor1 = ain0Value
+            
+            airspeed_pressure0 = ain0Value*VOLTS_TO_PASCAL #- offset0
+            raw_pressure0      = airspeed_pressure0
+            _raw_airspeed0     = math.sqrt( airspeed_pressure0 * _ratio)
+            _airspeed0         = 0.7 * _airspeed0  +  0.3 * _raw_airspeed0;
+            msg.sensor1 = _airspeed0
 
             ain2bits, = d.getFeedback(u3.AIN(2)) # Read from raw bits from AIN2
             ain2Value = d.binaryToCalibratedAnalogVoltage(ain2bits, isLowVoltage=False, channelNumber=2)
+            
+
+
             msg.sensor2 = ain2Value
 
             lc.publish('vortex_sensor',msg.encode())
+
+
+
+
+
+
 
     except KeyboardInterrupt:
         exit(0)
