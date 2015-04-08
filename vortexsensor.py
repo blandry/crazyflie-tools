623f2656c29fa68043c5202a64908deead84f6c2
peteflorence <pete.florence@gmail.com>
2015-04-08 19:09:59 -0400

    second attempt

--- vortexsensor.py
+++ vortexsensor.py
@@ -13,28 +13,22 @@ _airspeed2 = 0
 # Calibrate by taking 2000 measurements and taking simple average
 ain0offset = 0
 ain2offset = 2
-for i in range(1,2000):
-    ain0bits, = d.getFeedback(u3.AIN(0)) # Read from raw bits from AIN0
-    ain0Value = d.binaryToCalibratedAnalogVoltage(ain0bits, isLowVoltage=False, channelNumber=0)
-    ain0offset = (ain0offset + ain0value) / i
-
-    ain2bits, = d.getFeedback(u3.AIN(2)) # Read from raw bits from AIN0
-    ain2Value = d.binaryToCalibratedAnalogVoltage(ain2bits, isLowVoltage=False, channelNumber=0)
-    ain2offset = (ain2offset + ain2value) / i
-
-
 
 
 if __name__=="__main__":
     d = u3.U3()
     lc = lcm.LCM()
 
+    for i in range(1,2000):
+        ain0bits, = d.getFeedback(u3.AIN(0)) # Read from raw bits from AIN0
+        ain0Value = d.binaryToCalibratedAnalogVoltage(ain0bits, isLowVoltage=False, channelNumber=0)
+        ain0offset = (ain0offset + ain0value) / i
+
+        ain2bits, = d.getFeedback(u3.AIN(2)) # Read from raw bits from AIN0
+        ain2Value = d.binaryToCalibratedAnalogVoltage(ain2bits, isLowVoltage=False, channelNumber=0)
+        ain2offset = (ain2offset + ain2value) / i
+
     try:
-
-
-        # implement offset
-
-
 
         while True:
             msg = vortex_sensor_t()
