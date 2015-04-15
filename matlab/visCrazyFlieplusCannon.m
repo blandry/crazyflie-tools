07b1cae422c3433a6a0f5a526c848c5fa2c29b70
peteflorence <pete.florence@gmail.com>
2015-04-15 18:15:41 -0400

    TwoRobotViconCoder

--- matlab/visCrazyFlieplusCannon.m
+++ matlab/visCrazyFlieplusCannon.m
@@ -1,3 +1,8 @@
 options.floating=true;
 r=RigidBodyManipulator('crazyflie.urdf',options); 
 r=r.addRobotFromURDF('cannon.urdf', [], [], options);
+v = r.constructVisualizer();
+vicon_frame = LCMCoordinateFrame('VortexCannon-40gal',TwoRobotViconCoder,'x');
+vicon_frame.addTransform(AffineTransform(vicon_frame,v.getInputFrame,eye(6),zeros(6,1)));
+v = v.inInputFrame(vicon_frame);
+runLCM(v,[]);
